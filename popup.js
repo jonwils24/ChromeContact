ad899f266f558a87d16e73491635abcea51da06a
Jonathan Wilson <wilsonwjonathan@gmail.com>
2014-11-02 11:59:29 -0800

    cleaned js file

--- popup.js
+++ popup.js
@@ -1,22 +1,3 @@
-// <<<<<<< HEAD
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   var activeTab = tabs[0];
-//   var tabId = activeTab.id;
-//   var tabUrl = activeTab.url;
-//   var tabName = activeTab.title;
-//
-//   var regex = /www\.(.*)\.com/g;
-//
-//   var domain = regex.exec(tabUrl);
-//
-//
-//
-//   alert(domain[1]);
-// });
-//
-//
-// alert($);
-// =======
 chrome.tabs.getSelected(null,function(tab) {
     var tablink = tab.url;
     var parser = document.createElement('a');
