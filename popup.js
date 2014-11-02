d297cc990e88d1aeb552ed4df4a5438cda0bfc83
Richard Yang <elyrly@gmail.com>
2014-11-02 13:40:23 -0800

    mailgun working

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ chrome.tabs.getSelected(null,function(tab) {
     }else{
      var companyName = arr[1]
     };
-     $.ajax({
+      $.ajax({
        url: 'http://api.crunchbase.com/v/2/organization/'+companyName+'?user_key=bd342b3149f49c9b71a146761bbe0604',
        data: {
           format: 'json'
@@ -23,7 +23,26 @@ chrome.tabs.getSelected(null,function(tab) {
          $(".image").append("<image src=http://www.crunchbase.com/organization/" + companyName + "/primary-image/raw>"),
          response.data.relationships.founders.items.forEach(function(obj) { $("#popup").append("<li>"+obj.name+"</li>")}),
          $("#summary").append(response.data.properties.short_description),
-         response.data.relationships.founders.items.forEach(function(obj){var nameArr = obj.name.split(" ");var email = nameArr[0][0] + nameArr[1]+"@"+companyName+".com"; $('#email').append("<li>"+email.toLowerCase()+"</li>") } )
+         response.data.relationships.founders.items.forEach(function(obj){
+           var nameArr = obj.name.split(" ");
+           var email = nameArr[0][0] + nameArr[1]+"@"+companyName+".com";
+           $.ajax({
+             type: "GET",
+             url: 'https://api.mailgun.net/v2/address/validate?callback=?',
+             data: { address: email, api_key: "pubkey-98b8d4b703e95b501f8f54b5b8c5a261"},
+             dataType: "jsonp",
+             crossDomain: true,
+             success: function(data, status_text) {
+               console.log("get here")
+               if(data.is_valid){
+                  $('#email').append("<li>"+email.toLowerCase()+"</li>")
+               }else{
+                  $('#email').append("<li>"+email.toLowerCase()+"</li>")
+               }
+             },
+          });
+         })
+
        },
        type: 'GET'
     });
