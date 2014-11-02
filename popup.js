8e03a33b61bdad40022f9da6d86290c657c909eb
Richard Yang <elyrly@gmail.com>
2014-11-02 11:51:06 -0800

    icons and email

--- popup.js
+++ popup.js
@@ -22,6 +22,7 @@ chrome.tabs.getSelected(null,function(tab) {
     var parser = document.createElement('a');
     parser.href = tablink
     var domainName = parser.hostname
+
     var arr = domainName.split(".")
     if (arr[1] === ("com") | ("io") | ("org") | ("edu") | ("co") | ("us")) {
      var companyName = arr[0]
@@ -40,7 +41,8 @@ chrome.tabs.getSelected(null,function(tab) {
        success: function(response) {
          $(".image").append("<image src=http://www.crunchbase.com/organization/" + companyName + "/primary-image/raw>"),
          response.data.relationships.founders.items.forEach(function(obj) { $("#popup").append("<li>"+obj.name+"</li>")}),
-         $("#summary").append(response.data.properties.short_description);
+         $("#summary").append(response.data.properties.short_description),
+         response.data.relationships.founders.items.forEach(function(obj){var nameArr = obj.name.split(" ");var email = nameArr[0][0] + nameArr[1]+"@"+companyName+".com"; $('#email').append("<li>"+email.toLowerCase()+"</li>") } )
        },
        type: 'GET'
     });
